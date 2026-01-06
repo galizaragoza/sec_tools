5f54a659f148260e3785b1418b7bdbc51b47fbc0
Mario Hinojosa Freire <124710409+galizaragoza@users.noreply.github.com>
2026-01-06 13:21:56 +0000

    Added percentage of work done functionality to nmapper.sh

--- nmapper.sh
+++ nmapper.sh
@@ -3,10 +3,14 @@
 echo "Analizando la lista de IPs proporcionada"
 for i in $@
 do
-        echo "Analizando la IP: $i\n\n\n"
-        nmap -p 0-65535 --open -A -T0 -sS "$i" -oX nmap_"$i".xml && xsltproc nmap_"$i".xml -o nmap_"$i".html 2 >
- error.log
+        total=$#
+        counter=1
+        perc=$((counter*100/total))
+        echo "Analizando la IP: $i\n\n\n $counter de $total ($perc %)"
+        nmap --top-ports 10000 --open -A -T3 -v -sT -Pn "$i" -oX nmap_"$i".xml && xsltproc nmap_"$i".xml -o nmap
+_"$i".html 2>>errors.log
         echo "Análisis de $i completo\n\n\n"
+        counter=$((counter+1))
 
 done
 
