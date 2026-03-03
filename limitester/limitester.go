7d55f07e9cd9ba55c31fc0a6b0b513c3b69a6f3e
Mario <galizaragozadev@gmail.com>
2026-03-03 12:57:34 +0100

    added a time diff info log and post and both (get and post) options

--- limitester/limitester.go
+++ limitester/limitester.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"runtime"
 	"sync"
+	"time"
 
 	"github.com/jpillora/opts"
 )
 
 type Config struct {
 	URL      string `opts:"short=u, help=Set the URL to test"`
+	Method   string `opts:"short=m, help=Use GET, POST, or both (e.g. -m both, -m get)"`
 	Packets  int    `opts:"short=p, help=Amount of packets to be sent (defaults to 1000)"`
 	Parallel int    `opts:"short=w, help=Amount of parellel workers"`
 	Threads  int    `opts:"short=t, help=Amount of system threads"`
@@ -29,7 +32,7 @@ func checkOpts(c Config) (Config, error) {
 	return c, nil
 }
 
-func testLimits(c Config) error {
+func getLimits(c Config) error {
 	var worker sync.WaitGroup
 	jobs := make(chan int, c.Packets)
 
@@ -61,7 +64,41 @@ func testLimits(c Config) error {
 	return nil
 }
 
+func postLimits(c Config) error {
+	var worker sync.WaitGroup
+	jobs := make(chan int, c.Packets)
+
+	for w := 1; w <= c.Parallel; w++ {
+		worker.Add(1)
+		go func(workerID int) {
+			defer worker.Done()
+			for j := range jobs {
+				req, err := http.Post("POST", c.URL, &io.LimitedReader{})
+				if err != nil {
+					fmt.Printf("Worker %d: Error testing limits: %d: %v\n", workerID, j, err)
+					continue
+				}
+				fmt.Printf("Worker %d | Request %d of %d: %s\n", workerID, j, c.Packets, req.Status)
+				req.Body.Close()
+			}
+		}(w)
+	}
+
+	count := 0
+	for i := c.Packets; i > 0; i-- {
+		req, err := http.Get(c.URL)
+		if err != nil {
+			return fmt.Errorf("Error testing limits:", err)
+		}
+		count += 1
+		fmt.Printf("Request %d of %d: %#v\n", count, c.Packets, req.Status)
+	}
+	return nil
+}
+
 func main() {
+	start := time.Now()
+	fmt.Printf("Starting at %#v", start)
 	c := Config{}
 
 	opts.Parse(&c)
@@ -73,5 +110,16 @@ func main() {
 		fmt.Println("Error validating config:", err)
 	}
 
-	testLimits(c)
+	switch c.Method {
+	case "get":
+		getLimits(c)
+	case "post":
+		postLimits(c)
+	case "both":
+		getLimits(c)
+		postLimits(c)
+	}
+
+	elapsed := time.Since(start)
+	fmt.Printf("Did %d requests in %#v minutes", c.Packets, elapsed)
 }
