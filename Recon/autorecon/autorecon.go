3014ab64964f741b8679f10800aa917d8fc01425
Mario <galizaragozadev@gmail.com>
2026-03-06 16:47:30 +0100

    0.1

--- Recon/autorecon/autorecon.go
+++ Recon/autorecon/autorecon.go
@@ -2,90 +2,214 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+
+	"golang.org/x/sync/errgroup"
 
 	"github.com/jpillora/opts"
 )
 
 type Config struct {
 	// Info
-	Stealth int    `opts:"short=s, help=Level of stealth wanted in the recon (1 is less agressive, 5 is most agressive), defaults to 1"`
-	IP      string `opts:"short=i, help=IP of the target, required for Nmap"`
+	Stealth int    `opts:"short=s, help=Level of stealth wanted in the recon (1 is less) defaults to 1"`
+	IP      string `opts:"short=i, help=IP of the target (required for Nmap)"`
 	URL     string `opts:"short=u, help=Target URL"`
-	Domain  string `opts:"short=d, help=Target domain, required for dnsrecon"`
-	// Tool selection
-	Full     bool `opts:"short=f, help=Uses Nikto, DNSrecon and Nmap (NSE has to be selected separately)"`
-	Nikto    bool `opts:"help=Use nikto in the recon"`
-	Dnsrecon bool `opts:"help=Use dnsrecon in the recon"`
-	Nmap     bool `opts:"help=Use nmap in the recon"`
+	Domain  string `opts:"short=d, help=Target domain (required for dnsrecon)"`
 	// NSE
-	NSE    bool   `opts:"help=Use NSE (nmap scripting engine), the name of a script or category has to be provided"`
+	NSE    bool   `opts:"help=Use NSE (nmap scripting engine) the name of a script or category has to be provided"`
 	Script string `opts:"help=Specify script or category to use with NSE"`
 }
 
-func getPath(isSet bool, tool string) (string, error) {
-	if isSet {
-		path, err := exec.LookPath(tool)
-		if err != nil {
-			return "", fmt.Errorf("[X] Error: %s is not installed\n\n", tool)
-		}
-		return path, nil
+func getPath(tool string) (string, error) {
+	path, err := exec.LookPath(tool)
+	if err != nil {
+		return "", fmt.Errorf("[X] Error: %s is not installed", tool)
 	}
-	return "", nil
+	return path, nil
 }
 
 func goNmap(c Config) error {
-	nmapPath, err := getPath(c.Nmap, "nmap")
+	fmt.Println("Running Nmap scan...")
+	nmapPath, err := getPath("nmap")
 	if err != nil {
 		return err
 	}
+	fmt.Printf("Nmap is installed, %#v\n", nmapPath)
+	xsltprocPath, err := getPath("xsltproc")
+	if err != nil {
+		return err
+	}
+	fmt.Printf("xsltproc is installed, %#v\n", xsltprocPath)
 
-	var cmd []string
+	reportName := "autorecon_nmapScan.xml"
+	parsedReport := "autorecon_nmapScan.html"
+	var args []string
 
 	if c.IP == "" {
-		return fmt.Errorf("[X] Error: Nmap is being called but no IP was provided. Value of IP %#v\n\n", c.IP)
+		return fmt.Errorf("[X] Error: Nmap is being called but no IP was provided. Value of IP %#v", c.IP)
 	}
 	switch c.Stealth {
 	case 1:
-		cmd = []string{
-			nmapPath, "--top-ports", "100", "-sS", "--stats-every=10", "-T1", c.IP, "-oX", "autorecon_nmapScan.xml",
+		args = []string{
+			"--top-ports", "100", "-sS", "--stats-every=10", "-T1", c.IP, "-oX", reportName,
 		}
 	case 2:
-		cmd = []string{
-			nmapPath, "--top-ports", "1000", "-sS", "-sV", "--stats-every=10", "-T2", c.IP, "-oX", "autorecon_nmapScan.xml",
+		args = []string{
+			"--top-ports", "1000", "-sS", "-sV", "--stats-every=10", "-T2", c.IP, "-oX", reportName,
 		}
 	case 3:
-		cmd = []string{
-			nmapPath, "--top-ports", "10000", "-sT", "-sV", "-sC", "--stats-every=10", "-T3", c.IP, "-oX", "autorecon_nmapScan.xml",
+		args = []string{
+			"--top-ports", "10000", "-sT", "-sV", "-sC", "--stats-every=10", "-T3", c.IP, "-oX", reportName,
 		}
 	case 4:
-		cmd = []string{
-			nmapPath, "-p", "0-65535", "--open", "-sT", "-A", "--stats-every=10", "-T4", c.IP, "-oX", "autorecon_nmapScan.xml",
+		args = []string{
+			"-p", "0-65535", "--open", "-sT", "-A", "--stats-every=10", "-T4", c.IP, "-oX", reportName,
 		}
 	case 5:
-		cmd = []string{
-			nmapPath, "-p", "0-65535", "--open", "-sT", "-A", "--stats-every=10", "-T5", c.IP, "-oX", "autorecon_nmapScan.xml",
+		args = []string{
+			"-p", "0-65535", "--open", "-sT", "-A", "--stats-every=10", "-T5", c.IP, "-oX", reportName,
 		}
 	default:
 		return fmt.Errorf("[X] Error: The stealth value (1-5) provided was incorrect. Value of Stealth %#v", c.Stealth)
 	}
-	fmt.Printf("Scanning with nmap...\n%#v", cmd)
-	// exec.Command(130H15|, arg ...string)
+
+	cmd := exec.Command(nmapPath, args...)
+	fmt.Println("Scanning with nmap...")
+	err = cmd.Run()
+	if err != nil {
+		return fmt.Errorf("[X] Error: running the nmap scan:%#v", err)
+	}
+
+	fmt.Println("Done scanning, parsing report...")
+
+	cmd = exec.Command(xsltprocPath, reportName, "-o", parsedReport)
+	err = cmd.Run()
+	if err != nil {
+		return err
+	}
+	fmt.Println("Nmap scan is complete")
+
+	return nil
+}
+
+func goDNSrecon(c Config) error {
+	binPath, err := getPath("dnsrecon")
+	if err != nil {
+		return err
+	}
+	xsltprocPath, err := getPath("xsltproc")
+	if err != nil {
+		return err
+	}
+
+	reportName := "autorecon_DNSrecon.xml"
+	parsedReport := "autorecon_DNSrecon.html"
+
+	fmt.Printf("DNSrecon is installed, %#v", binPath)
+
+	if c.Domain == "" {
+		fmt.Printf("Domain is required to use DNSrecon, specify one, current value is %#v", c.Domain)
+	}
+
+	args := []string{
+		"-d", c.Domain, "-absykez", "-x", "-t", "std",
+	}
+	cmd := exec.Command(binPath, args...)
+	fmt.Println("Running DNSrecon part 1/2...")
+	err = cmd.Run()
+	if err != nil {
+		return fmt.Errorf("[X] Error: Error in DNS recon with args %#v", args)
+	}
+	cmd = exec.Command(xsltprocPath, reportName, "-o", parsedReport)
+	err = cmd.Run()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func goWhatWeb(c Config) error {
+	binPath, err := getPath("whatweb")
+	if err != nil {
+		return err
+	}
+	var args []string
+	reportName := "autorecon_WhatWeb"
+
+	if c.URL == "" {
+		return fmt.Errorf("[X] Error: URL was not specified")
+	}
+
+	switch c.Stealth {
+	case 1:
+		args = []string{"-a", "1", "--colour=auto", "-v", c.URL}
+	case 2:
+		args = []string{"-a", "2", "--colour=auto", "-v", c.URL}
+	case 3:
+		args = []string{"-a", "3", "--colour=auto", "-v", c.URL}
+	case 4:
+		args = []string{"-a", "4", "--colour=auto", "-v", c.URL}
+	case 5:
+		args = []string{"-a", "4", "--colour=auto", "-v", c.URL}
+	}
+	cmd := exec.Command(binPath, args...)
+	fmt.Println("Fingerprinting web technologies...")
+	out, err := cmd.Output()
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(reportName, out, 0o444)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func goWafW00f(c Config) error {
+	binPath, err := getPath("wafw00f")
+	if err != nil {
+		return err
+	}
+	reportName := "autorecon_wafw00f.csv"
+
+	if c.URL == "" {
+		return fmt.Errorf("URL value is empty, but it is required")
+	}
+
+	args := []string{"-a", "-o", reportName, "-f", "json", "-l"}
+
+	cmd := exec.Command(binPath, args...)
+	fmt.Println("Scanning for WAF...")
+	err = cmd.Run()
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func main() {
-	c := Config{
-		Stealth: 1,
-		// SOLO PARA TESTING DE AQUÍ PARA ABAJO
-		IP: "127.0.0.1",
-	}
+	c := Config{}
 
 	opts.Parse(&c)
 
-	if c.Full {
-		c.Nikto, c.Dnsrecon, c.Nmap = true, true, true
-	}
-
 	fmt.Printf("Current configuration: %+v\n\n", c)
+
+	g := new(errgroup.Group)
+
+	g.Go(func() error {
+		return goNmap(c)
+	})
+
+	g.Go(func() error {
+		return goDNSrecon(c)
+	})
+
+	g.Go(func() error {
+		return goWhatWeb(c)
+	})
+
+	if err := g.Wait(); err != nil {
+		panic(err)
+	}
 }
--- Recon/autorecon/go.mod
+++ Recon/autorecon/go.mod
@@ -7,4 +7,6 @@ require (
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/jpillora/opts v1.3.1 // indirect
 	github.com/posener/complete v1.2.3 // indirect
+	github.com/wamuir/go-xslt v0.1.5 // indirect
+	golang.org/x/sync v0.19.0 // indirect
 )
